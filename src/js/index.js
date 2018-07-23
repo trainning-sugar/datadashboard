8611875a27b72029f3dd447d390aeb0b51e7e6da
trainning-sugar <mlvilchezp@gmail.com>
2018-07-23 05:12:00 -0500

    agregando el inicio de proyecto

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+ const buttonMenu = document.getElementById('button-menu');
+buttonMenu.addEventListener('click', e => {
+  console.log(e.target);
+  console.log(e.target.nextElementSibling);
+  e.target.nextElementSibling.classList.toggle('show');
+}) 
