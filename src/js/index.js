aabfbe07c35c3408c7dbfc298b853b40f4590237
trainnig-sugar <mlvilchezp@gmail.com>
2018-08-01 05:54:45 -0500

    agregando el menu first mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
  const buttonMenu = document.getElementById('button-menu');
-buttonMenu.addEventListener('click', e => {
-  console.log(e.target);
-  console.log(e.target.nextElementSibling);
-  e.target.nextElementSibling.classList.toggle('show');
-}) 
+ buttonMenu.addEventListener('click', _ => {
+   const navbarContent = document.getElementById('collapse-navbar-content');
+   navbarContent.classList.toggle('show');
+ })
 
 
-//url https://api.laboratoria.la/campuses
-
+ //url https://api.laboratoria.la/campuses
--- src/js/utils/media.js
+++ src/js/utils/media.js
@@ -1,6 +1,23 @@
-const handlerMedia = e => {
+/* var mediaQueryList = window.matchMedia("(min-width: 768px)"); // Create the query list.
+function handleOrientationChange(mediaQueryList) {
  const flexContainerNav = document.getElementById('navbar-content');
  e.matches ? flexContainerNav.classList.remove('flex-column') : null;
+} // Define a callback function for the event listener.
+mediaQueryList.addListener(handleOrientationChange); // Add the callback function as a listener to the query list.
+
+handleOrientationChange(mediaQueryList); // Run the orientation change handler once. */
+
+
+/* function myFunction(x) {
+ const flexContainerNav = document.getElementById('navbar-content');
+ console.log(x);
+ if (x.matches) { // If media query matches
+  flexContainerNav.classList.remove('flex-column')
+ } else {
+  flexContainerNav.classList.add('flex-column')
+ }
 }
-handlerMedia(e);
-window.matchMedia("(min-width: 768px)").addListener(handlerMedia);
+
+var mediaQueryList = window.matchMedia("(min-width: 768px)")
+myFunction(mediaQueryList) // Call listener function at run time
+mediaQueryList.addListener(myFunction) // Attach listener function on state changes */
