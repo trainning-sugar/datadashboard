69422842aba4772622ba2649114dc48dacaf4e0a
trainnig-sugar <mlvilchezp@gmail.com>
2018-08-01 07:18:39 -0500

    creacion de la funcion getAjaxRequest y haciendo request de los breakpoints campuses y cohorts

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,24 @@
  })
 
 
- //url https://api.laboratoria.la/campuses
+ const getAjaxRequest = url => {
+   return new Promise((resolve, reject) => {
+     const xhr = new XMLHttpRequest();
+     xhr.open('GET', url);
+     xhr.addEventListener('load', e => {
+       if (e.target.readyState === 4 && e.target.status !== 200) return reject(new Error(`Error loading JSON from ${url} ${e.status}`))
+       resolve(JSON.parse(e.target.responseText));
+     })
+     xhr.send();
+   })
+ }
+
+
+ Promise.all([getAjaxRequest('https://api.laboratoria.la/campuses'), getAjaxRequest('https://api.laboratoria.la/cohorts')])
+   .then((data) => {
+     console.log(data);
+   })
+ //https://api.laboratoria.la/cohorts
+ //https://api.laboratoria.la/cohorts/lim-2017-09-bc-core-am/progress
+ //https://api.laboratoria.la/cohorts/lim-2017-09-bc-core-am/progress
+ //https://api.laboratoria.la/cohorts/lim-2017-09-bc-core-am/users
