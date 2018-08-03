46122f5fb474b405caa9f1ee1f1b1362fd72a240
trainnig-sugar <mlvilchezp@gmail.com>
2018-08-02 21:53:24 -0500

    add input order by, order direction and search in html, add case in change event, add submit event, add data file and processCohortData function

--- src/js/data.js
+++ src/js/data.js
@@ -1,5 +1,5 @@
-function greeting() {
- console.log('hola');
-};
-
-module.exports = greeting;
+window.processCohortData = (options) => {
+ console.log(options);
+ console.log(options.cohortData.users);
+ console.log(options.cohortData.progress);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,17 @@ const path = {
   year: null
 }
 
+const options = {
+  cohort: null,
+  cohortData: {
+    users: null,
+    progress: null
+  },
+  orderBy: null,
+  orderDirection: null,
+  search: null
+};
+
 const renderCampus = (campuses, id) => {
   const selectCampus = document.getElementById(id)
   campuses.filter(campus => campus.active)
@@ -37,12 +48,18 @@ const searchCohorts = (sede, year) => {
   }
 }
 
+const filterCohort = (cohorts, value, options) => {
+  options.cohort = cohorts.filter(cohort => cohort.id === value)[0];
+  return options;
+}
+
 Promise.all([getAjaxRequest('https://api.laboratoria.la/campuses'), getAjaxRequest('https://api.laboratoria.la/cohorts')])
   .then(data => {
     const [campuses, allCohorts] = data;
     path.allCohorts = allCohorts;
     renderCampus(campuses, 'campuses');
-  });
+  })
+  .catch(err => console.error(err));
 
 document.addEventListener('DOMContentLoaded', _ => {
 
@@ -55,6 +72,7 @@ document.addEventListener('DOMContentLoaded', _ => {
   });
 
   form.addEventListener('change', e => {
+    console.log(e);
     switch (true) {
       case (e.target.id === 'campuses'):
         path.sede = e.target.value;
@@ -64,14 +82,38 @@ document.addEventListener('DOMContentLoaded', _ => {
         path.year = e.target.value;
         searchCohorts(path.sede, path.year);
         break;
+      case (e.target.id === 'cohorts'):
+        filterCohort(path.allCohorts, e.target.value, options);
+        console.log(e.target.value);
+        Promise.all([getAjaxRequest(`https://api.laboratoria.la/cohorts/${e.target.value}/users`), getAjaxRequest(`https://api.laboratoria.la/cohorts/${e.target.value}/progress`)])
+          .then(data => {
+            options.cohortData.users = data[0];
+            options.cohortData.progress = data[1];
+          })
+          .catch(err => console.error(err))
+        break;
+      case (e.target.id === "orderBy"):
+        console.log(e.target.value);
+        options.orderBy = e.target.value
+        break;
+      case (e.target.id === "asc"):
+        options.orderDirection = e.target.value
+        break;
+      case (e.target.id === "desc"):
+        options.orderDirection = e.target.value
+        break;
+      case (e.target.id === "search"):
+        options.search = e.target.value
+        break;
     }
   })
 
 
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    setTimeout(() => {
+      processCohortData(options);
+    }, 3000)
+    e.target.reset()
+  })
 })
-
-
-//https://api.laboratoria.la/cohorts
-//https://api.laboratoria.la/cohorts/lim-2017-09-bc-core-am/progress
-//https://api.laboratoria.la/cohorts/lim-2017-09-bc-core-am/progress
-//https://api.laboratoria.la/cohorts/lim-2017-09-bc-core-am/users
